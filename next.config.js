a26ba76b43675ac9d2ab2e5b8461beafb81b29e9
c-jeongyyun <99jenny0614@naver.com>
2026-01-27 20:29:52 +0900

    fix: add all subdomains of googleusercontent.com to allow-list

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-
-// TODO : Next.js 업데이트 후 최신 문법 적용해야함. - images.remotePatterns
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['www.notion.so', 'lh1.googleusercontent.com','lh2.googleusercontent.com','lh3.googleusercontent.com','lh4.googleusercontent.com','lh5.googleusercontent.com','lh6.googleusercontent.com','s3-us-west-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com', // accept all subdomains of googleusercontent.com
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-us-west-2.amazonaws.com',
+      },
+    ],
   },
 }
+
+module.exports = nextConfig
