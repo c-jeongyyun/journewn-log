f644eab633c13d4ba2d46b83b07c0a5278cb6ea4
c-jeongyyun <99jenny0614@naver.com>
2026-01-27 19:43:00 +0900

    bugfix: update image url allow-list

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+
+// TODO : Next.js 업데이트 후 최신 문법 적용해야함. - images.remotePatterns
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ['www.notion.so', 'lh1.googleusercontent.com','lh2.googleusercontent.com','lh3.googleusercontent.com','lh4.googleusercontent.com','lh5.googleusercontent.com','lh6.googleusercontent.com','s3-us-west-2.amazonaws.com'],
   },
 }
